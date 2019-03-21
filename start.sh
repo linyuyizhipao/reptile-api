0f582aa9e6d91b640cfb62b897502a9f96375e59
胡爽 <13147199592@163.com>
2019-03-21 16:21:34 +0800

    优化jiaoben

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #/bin/sh
-
+echo '' > ./log.txt
 nohup php ./run.php 1>./log.txt &
 
-echo "爬虫启动成功，请用 tail -f ./log.txt 查看进度"
+echo "爬虫启动成功，请用 tail -f ./log.txt 查看进度 命令提示:  ps -ef | grep php   查出脚本开启的run.php pid  kill"
