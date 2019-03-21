29269fa0bb9866bb5c7abc1a95c9696a8915bfc0
胡爽 <13147199592@163.com>
2019-03-21 17:29:26 +0800

    tiaoyou

--- run.php
+++ run.php
@@ -5,13 +5,16 @@ require './vendor/autoload.php';
 require './companyBaseData.php';
 require './companyDetail.php';
 
-
+$time = date("Y-m-d H:i:s",time());
+var_dump($time);
 
 $app = new ReptilePage();
 $app->start();
 
 $app2 = new CompanyDetail();
-$app->start();
+$app2->start();
+
+echo '爬去完毕，爬去完毕，爬去完毕';
 
 
 
